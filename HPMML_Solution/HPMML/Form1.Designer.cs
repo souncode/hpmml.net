f466c9fd9a3183eaaa487cd895995b0c0a0c55d3
soun <xathutihon@gmail.com>
2024-10-10 03:08:53 +0700

    2nd commit

---
+++ HPMML_Solution/HPMML/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace HPMML
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelButton = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnControl = new System.Windows.Forms.Button();
+            this.btnMonitor = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnTrigger = new System.Windows.Forms.Button();
+            this.pbStt = new System.Windows.Forms.PictureBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cbPlc = new System.Windows.Forms.ComboBox();
+            this.cbDevices = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbFail = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.pbRaw = new System.Windows.Forms.PictureBox();
+            this.timerGetTrigger = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panelButton.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStt)).BeginInit();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.panelButton);
+            this.panel1.Controls.Add(this.panel2);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panelButton
+            // 
+            this.panelButton.Controls.Add(this.btnExit);
+            this.panelButton.Controls.Add(this.btnSetting);
+            this.panelButton.Controls.Add(this.btnControl);
+            this.panelButton.Controls.Add(this.btnMonitor);
+            this.panelButton.Controls.Add(this.btnHome);
+            resources.ApplyResources(this.panelButton, "panelButton");
+            this.panelButton.Name = "panelButton";
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Image = global::HPMML.Properties.Resources.leave;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSetting
+            // 
+            resources.ApplyResources(this.btnSetting, "btnSetting");
+            this.btnSetting.Image = global::HPMML.Properties.Resources.settings;
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            // 
+            // btnControl
+            // 
+            resources.ApplyResources(this.btnControl, "btnControl");
+            this.btnControl.Image = global::HPMML.Properties.Resources.settings_sliders;
+            this.btnControl.Name = "btnControl";
+            this.btnControl.UseVisualStyleBackColor = true;
+            // 
+            // btnMonitor
+            // 
+            resources.ApplyResources(this.btnMonitor, "btnMonitor");
+            this.btnMonitor.Image = global::HPMML.Properties.Resources.browser;
+            this.btnMonitor.Name = "btnMonitor";
+            this.btnMonitor.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            resources.ApplyResources(this.btnHome, "btnHome");
+            this.btnHome.Image = global::HPMML.Properties.Resources.home;
+            this.btnHome.Name = "btnHome";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HPMML.Properties.Resources.logop;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.DimGray;
+            this.panelTop.Controls.Add(this.btnStart);
+            this.panelTop.Controls.Add(this.btnProcess);
+            this.panelTop.Controls.Add(this.btnTrigger);
+            this.panelTop.Controls.Add(this.pbStt);
+            this.panelTop.Controls.Add(this.btnConnect);
+            resources.ApplyResources(this.panelTop, "panelTop");
+            this.panelTop.Name = "panelTop";
+            // 
+            // btnStart
+            // 
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Image = global::HPMML.Properties.Resources.start;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnProcess
+            // 
+            resources.ApplyResources(this.btnProcess, "btnProcess");
+            this.btnProcess.Image = global::HPMML.Properties.Resources.Process;
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnTrigger
+            // 
+            resources.ApplyResources(this.btnTrigger, "btnTrigger");
+            this.btnTrigger.Image = global::HPMML.Properties.Resources.pic;
+            this.btnTrigger.Name = "btnTrigger";
+            this.btnTrigger.UseVisualStyleBackColor = true;
+            this.btnTrigger.Click += new System.EventHandler(this.btnTrigger_Click);
+            // 
+            // pbStt
+            // 
+            this.pbStt.Image = global::HPMML.Properties.Resources.stop;
+            resources.ApplyResources(this.pbStt, "pbStt");
+            this.pbStt.Name = "pbStt";
+            this.pbStt.TabStop = false;
+            // 
+            // btnConnect
+            // 
+            resources.ApplyResources(this.btnConnect, "btnConnect");
+            this.btnConnect.Image = global::HPMML.Properties.Resources.link;
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.Controls.Add(this.comboBox2);
+            this.panel6.Controls.Add(this.cbPlc);
+            this.panel6.Controls.Add(this.cbDevices);
+            this.panel6.Controls.Add(this.label7);
+            this.panel6.Controls.Add(this.label8);
+            this.panel6.Controls.Add(this.lbFail);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Controls.Add(this.lbPass);
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Controls.Add(this.pbResult);
+            this.panel6.Controls.Add(this.pbRaw);
+            resources.ApplyResources(this.panel6, "panel6");
+            this.panel6.Name = "panel6";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // cbPlc
+            // 
+            this.cbPlc.FormattingEnabled = true;
+            resources.ApplyResources(this.cbPlc, "cbPlc");
+            this.cbPlc.Name = "cbPlc";
+            // 
+            // cbDevices
+            // 
+            this.cbDevices.FormattingEnabled = true;
+            resources.ApplyResources(this.cbDevices, "cbDevices");
+            this.cbDevices.Name = "cbDevices";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // lbFail
+            // 
+            resources.ApplyResources(this.lbFail, "lbFail");
+            this.lbFail.Name = "lbFail";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // lbPass
+            // 
+            resources.ApplyResources(this.lbPass, "lbPass");
+            this.lbPass.Name = "lbPass";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // pbResult
+            // 
+            this.pbResult.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pbResult, "pbResult");
+            this.pbResult.Name = "pbResult";
+            this.pbResult.TabStop = false;
+            // 
+            // pbRaw
+            // 
+            this.pbRaw.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pbRaw, "pbRaw");
+            this.pbRaw.Name = "pbRaw";
+            this.pbRaw.TabStop = false;
+            // 
+            // timerGetTrigger
+            // 
+            this.timerGetTrigger.Tick += new System.EventHandler(this.timerGetTrigger_Tick);
+            // 
+            // Dashboard
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Dashboard";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panelButton.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelTop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbStt)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.Button btnMonitor;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.PictureBox pbRaw;
+        private System.Windows.Forms.PictureBox pbStt;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnTrigger;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbFail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.ComboBox cbDevices;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cbPlc;
+        private System.Windows.Forms.Timer timerGetTrigger;
+    }
+}
+
---
+++ HPMML_Solution/HPMML/Form1.cs
@@ -0,0 +1,303 @@
+﻿using S7.Net;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using DirectShowLib;
+using System.Threading;
+using System.IO.Ports;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Drawing2D;
+
+namespace HPMML
+{
+    public partial class Dashboard : Form
+    {
+   
+        #region Struct
+        struct Video_Device
+        {
+
+            public string Device_Name;
+            public int Device_ID;
+            public Guid Identifier;
+
+
+            public Video_Device(int ID, string Name, Guid Identity = new Guid())
+            {
+                Device_ID = ID;
+                Device_Name = Name;
+                Identifier = Identity;
+            }
+
+            public override string ToString()
+            {
+                return String.Format("[{0}] {1}: {2}", Device_ID, Device_Name, Identifier);
+            }
+        }
+        #endregion
+
+        #region Variables
+        SerialPort serialPort = new SerialPort();
+        const string plcIP = "192.168.0.11";
+        const string arduinoPort = "COM3";
+        bool isAlarm;
+        bool isPass;
+        float failVl;
+        float passVl;
+        int passCount = 0;
+        int failCount = 0;
+        int charCount;
+        private Plc plc;
+        int charLimit;
+        bool lockC;
+        Bitmap currentFrame;
+        Bitmap TempFrame;
+        private VideoCapture _capture;
+        Rectangle selectionRect = new Rectangle();
+        System.Drawing.Point startPoint;
+        Video_Device[] WebCams;
+        public Dashboard()
+        {
+            InitializeComponent();
+            SetRoundForm();
+        }
+        #endregion
+
+        #region Function
+        private void SetRoundForm()
+        {
+            GraphicsPath formShape = new GraphicsPath();
+            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+            int radius = 20;
+            int diameter = radius * 2;
+
+            // Vẽ các góc mềm mại của form
+            formShape.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90); // Top left corner
+            formShape.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90); // Top right corner
+            formShape.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90); // Bottom right corner
+            formShape.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90); // Bottom left corner
+            formShape.CloseFigure();
+
+            // Gán hình dạng đã tạo cho form
+            this.Region = new Region(formShape);
+        }
+        void getDevice()
+        {
+            DsDevice[] _SystemCamereas = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            WebCams = new Video_Device[_SystemCamereas.Length];
+            for (int i = 0; i < _SystemCamereas.Length; i++)
+            {
+                WebCams[i] = new Video_Device(i, _SystemCamereas[i].Name, _SystemCamereas[i].ClassID);
+                cbDevices.Items.Add(WebCams[i].ToString());
+            }
+            if (cbDevices.Items.Count > 0)
+            {
+                cbDevices.SelectedIndex = 1;
+            }
+        }
+        void disableBtn(bool en)
+        {
+            btnProcess.Enabled = !en;
+            btnTrigger.Enabled = !en;
+            btnStart.Enabled = !en;
+            btnHome.Enabled = !en;
+            btnMonitor.Enabled = !en;
+            btnSetting.Enabled = !en;
+            btnControl.Enabled = !en;
+        }
+        void connectPLC()
+        {
+            plc = new Plc(CpuType.S71500, plcIP, 0, 0);
+            plc.Open();
+            string plcIp = plc.IP;
+            cbPlc.Text = plcIp;
+            if (plc.IsConnected)
+            {
+                MessageBox.Show("PLC Connected");
+            }
+            else
+            {
+                MessageBox.Show("Fail to connect to PLC");
+            }
+        }
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            getDevice();
+            disableBtn(true);
+           pbResult.Location = new Point(0, 0);
+            pbResult.BackColor = Color.Transparent;
+
+
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            Mat frame = new Mat();
+            _capture.Retrieve(frame);
+            Image<Rgba, byte> image = frame.ToImage<Rgba, byte>().Flip(Emgu.CV.CvEnum.FlipType.None);
+            currentFrame = image.ToBitmap();
+        }
+        void connectCamera()
+        {
+            _capture = new VideoCapture(cbDevices.SelectedIndex);
+            _capture.SetCaptureProperty(CapProp.FrameWidth, 2560);
+            _capture.SetCaptureProperty(CapProp.FrameHeight, 1440);
+            _capture.SetCaptureProperty(CapProp.Fps, 100);
+            _capture.ImageGrabbed += ProcessFrame;
+            _capture.Start();
+            if (_capture.IsOpened)
+            {
+                MessageBox.Show("Camera Connected");
+                btnTrigger.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Fail to connect to Camera");
+            }
+        }
+        private void saveImage(bool isPass)
+        {
+
+            if (isPass)
+            {
+                try
+                {
+                    string currentTime = DateTime.Now.ToString("ddMMyy_hhmmss");
+                    pbRaw.Image.Save(@"D:\DataFinal\Pass\" + currentTime + "_Raw" + ".jpg");
+                    Console.WriteLine("Saved pass image");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fail to save");
+                }
+            }
+            else
+            {
+                try
+                {
+                    string currentTime = DateTime.Now.ToString("ddMMyy_hhmmss");
+                    pbRaw.Image.Save(@"D:\DataFinal\Fail\" + currentTime + "_Raw" + ".jpg");
+                    Console.WriteLine("Saved fail image");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fail to save");
+                }
+            }
+        }
+        private bool getResult(float passp, float failp)
+        {
+            if (passVl > failVl)
+            {
+                isPass = true;
+                passCount++;
+                lbPass.Text = passCount.ToString();
+                pbResult.Image = Properties.Resources.pass;
+                return true;
+
+            }
+            else
+            {
+                isPass = false;
+                failCount++;
+                lbFail.Text = failCount.ToString();
+                //    btnAlarm.PerformClick();
+                pbResult.Image = Properties.Resources.fail;
+                return false;
+
+            }
+        }
+
+        #endregion
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+           
+        }
+        private bool readFromPlc(string address)
+        {
+                object M01 = plc.Read(address);
+                bool M01c = Convert.ToBoolean(M01);
+                Console.WriteLine(M01c);
+                if (M01c)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                } 
+        }
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnTrigger_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (currentFrame != null)
+                {
+                    pbRaw.Image = currentFrame;
+                }
+                else
+                {
+                    MessageBox.Show("Null");
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+            //  plc.Write("M19.2", 1);
+            Thread.Sleep(50);
+            // plc.Write("M19.2", 0);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //connectPLC();
+            connectCamera();
+            disableBtn(false);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerGetTrigger.Start();
+            pbStt.Image = Properties.Resources.run;
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+
+            getResult(passVl,failVl);
+            saveImage(isPass);
+        }
+
+        private void timerGetTrigger_Tick(object sender, EventArgs e)
+        {
+            if (plc.IsConnected)
+            {
+                if (readFromPlc("M19.1"))
+                {
+                    //600 O
+                    Thread.Sleep(100);
+                    btnTrigger.PerformClick();
+                }
+            }
+            else
+            {
+                Console.WriteLine("PLC not connect");
+            }
+        }
+    }
+}
